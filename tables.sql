8bbb1d858ba547260a0b8c0f387767867f5e5f49
Collin Funk <collin.funk1@gmail.com>
2023-04-30 21:56:13 -0700

    Fixed tables and ERD for update report

--- tables.sql
+++ tables.sql
@@ -1,12 +1,4 @@
 
-DROP TABLE IF EXISTS employee_role;
-DROP TABLE IF EXISTS product_category;
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS supplier;
-DROP TABLE IF EXISTS supplier_product;
-
 CREATE TABLE employee_role (
 	role_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
 	role_name TEXT NOT NULL
@@ -64,13 +56,44 @@ CREATE TABLE supplier (
 	city TEXT NOT NULL
 );
 
-CREATE TABLE supplier_product (
-	supplier_product_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
-	product_id INTEGER NOT NULL,
+CREATE TABLE supply_order (
+	order_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	order_date TEXT NOT NULL,
+	shipment_date TEXT NOT NULL,
 	supplier_id INTEGER NOT NULL,
-	FOREIGN KEY(product_id)
-		REFERENCES product(product_id),
 	FOREIGN KEY(supplier_id)
 		REFERENCES supplier(supplier_id)
 );
+
+CREATE TABLE supply_order_product (
+	order_id INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	item_count INTEGER NOT NULL,
+	item_price FLOAT NOT NULL,
+	FOREIGN KEY(product_id)
+		REFERENCES product(product_id),
+	FOREIGN KEY(order_id)
+		REFERENCES supply_order(order_id)
+);
+
+CREATE TABLE customer_order (
+	order_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	order_date TEXT NOT NULL,
+	shipment_date TEXT NOT NULL,
+	customer_id INTEGER NOT NULL,
+	FOREIGN KEY(customer_id)
+		REFERENCES customer(customer_id)
+);
+
+CREATE TABLE customer_order_product (
+	customer_id INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	item_count INTEGER NOT NULL,
+	item_price FLOAT NOT NULL,
+	FOREIGN KEY(customer_id)
+		REFERENCES customer(customer_id),
+	FOREIGN KEY(product_id)
+		REFERENCES product(product_id)
+);
+
 
